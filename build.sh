cc0e465931086e04b75a0364555e84b11a3e2220
Cullen Drummey <46973458+cdrummey216@users.noreply.github.com>
2019-08-29 19:51:10 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 echo "building client..."
 cd client
-yarn  --production=true
+yarn
 REACT_APP_COMMIT_SHA=$SOURCE_VERSION \
 REACT_APP_API_HOST=$api_host \
 REACT_APP_API_PROTOCOL=$API_PROTOCOL \
@@ -10,5 +10,5 @@ cd ../
 
 echo "building server..."
 cd server
-yarn  --production=true
+yarn
 yarn build
